8a38060f2cdddf0e90de8bbe2d7f53b038181e09
Andrew Bennett <potatosaladx@gmail.com>
2011-03-28 08:45:44 -0600

    Clean up the Mongoid::Kraken module.

--- lib/mongoid/kraken.rb
+++ lib/mongoid/kraken.rb
@@ -23,27 +23,45 @@ module Mongoid
         self.class_eval { self.fields }
       end
 
-    private
+    protected
       def summon_the_kraken!
         unless self.kraken.nil?
           self.kraken.all_tentacles.flatten.each do |tentacle|
             tentacle_name = tentacle.name.to_s
             if tentacle_name.ends_with?("_id")
-              tentacle_reference = tentacle_name.gsub(/_id$/, '')
-              self.class_eval(<<CONTENTS)
-                referenced_in("#{tentacle_reference}", :class_name => "#{self.class.name}")
-CONTENTS
+              kraken_referenced_in(tentacle)
             elsif tentacle_name.ends_with?("_ids")
-              tentacle_references = tentacle_name.gsub(/_ids$/, '')
-              self.class_eval(<<CONTENTS)
-                references_and_referenced_in_many("#{tentacle_references.pluralize}", :class_name => "#{self.class.name}")
-CONTENTS
+              kraken_references_and_referenced_in_many(tentacle)
             else
-              self.class_eval "field(#{tentacle.name.to_s.inspect}, :type => #{tentacle.sucker})"
+              kraken_field(tentacle)
             end
           end
         end
         return self
+      end
+
+    private
+      def kraken_field(tentacle)
+        tentacle_name = tentacle.name.to_s
+        self.class_eval(%Q{
+          field("#{tentacle_name}", :type => #{tentacle.sucker})
+        })
+      end
+
+      def kraken_referenced_in(tentacle)
+        tentacle_name = tentacle.name.to_s
+        tentacle_reference = tentacle_name.gsub(/_id$/, '')
+        self.class_eval(%Q{
+          referenced_in("#{tentacle_reference}", :class_name => "#{self.class.name}")
+        })
+      end
+
+      def kraken_references_and_referenced_in_many(tentacle)
+        tentacle_name = tentacle.name.to_s
+        tentacle_references = tentacle_name.gsub(/_ids$/, '')
+        self.class_eval(%Q{
+          references_and_referenced_in_many("#{tentacle_references.pluralize}", :class_name => "#{self.class.name}")
+        })
       end
     end
   end
